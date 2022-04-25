b8a417239a2adbc032140a9dbe996be80e14355b
Jeff Stock <jantonstock@gmail.com>
2022-04-25 14:16:10 -0700

    Can extract text from a pdf.

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-#!/bin/python3
+# Resumationator
+#
+# Copyright (C) 22022 Jeff Stock
+# Author: Jeff Stock <jantonstock@gmail.com>
+# For license information, see LICENSE.TXT
 
+"""
+DOCSTRING
+"""
+from PyPDF2 import PdfFileReader
 import nltk
 
+
 def main():
-    return 'All your base are belong to us.'
+    with open('./static/resumeTemplate.pdf','rb') as file:
+        reader = PdfFileReader(file)
+        print(reader.numPages)
+        contents = reader.getPage(3).extractText().split('\n')
+        print(contents)
+        pass
 
 if __name__ == '__main__':
-    print(main())
+    main()
