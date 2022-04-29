d9909be13af0a9dc3720dfa6ca927684ecb464ee
Jeff Stock <jantonstock@gmail.com>
2022-04-29 01:16:29 -0700

    tagger implemented.

--- utils.py
+++ utils.py
@@ -14,12 +14,13 @@ import string
 import re
 import pandas as pd
 import nltk
+import numpy as np
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from PyPDF2 import PdfFileReader
 from nltk.tokenize import WhitespaceTokenizer
 from matplotlib import pyplot
-import numpy as np
+from nltk.corpus import brown
 
 # pulls the text out of a pdf
 def pdfParser(filename, pageNumber):
@@ -151,3 +152,28 @@ def makeHist(filepath):
     pyplot.hist(data[data['label'] == 'fit']['body_len'], bins,alpha=0.5, label='fit')
     pyplot.legend(loc='upper left')
     pyplot.show()
+
+def tagger(text):
+    # import a list of tagged sentences from the Brown corpus
+    # this will be used to train the tagger
+    brown_tagged_sents = brown.tagged_sents(categories='news')
+    brown_sents = brown.sents(categories='news')
+    # separate training sets from a test set to test effectiveness of
+    # tagger
+    size = int(len(brown_tagged_sents) * 0.9)
+    train_sents = brown_tagged_sents[:size]
+    test_sents = brown_tagged_sents[size:]
+    # combine taggers using a BACKOFF TAGGING
+    t0 = nltk.DefaultTagger('NN')
+    t1 = nltk.UnigramTagger(train_sents, backoff=t0)
+    t2 = nltk.BigramTagger(train_sents, backoff=t1)
+    # TrigramTagger can be added, but does not improve accuracy, so was
+    # intentionally ommitted
+    output = {}
+    count = 0
+    for sentence in text:
+        tokens = nltk.word_tokenize(sentence)
+        tagged = dict(t2.tag(tokens))
+        output[count] = tagged
+        count += 1
+    return output
