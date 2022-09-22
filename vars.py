c4abc40bdfc59601e77a8047835c7eabe0fa8eb1
Jeff Stock <jantonstock@gmail.com>
2022-09-22 13:57:40 -0700

    updated

--- vars.py
+++ vars.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>
 import nltk
+nltk.download('stopwords')
 
 eng_stop_words = nltk.corpus.stopwords.words('english')
 
